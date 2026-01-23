ae5a360eec846ab35ff51afff008268089c24a80
lhckmn <leander.hackmann@gmail.com>
2026-01-23 23:44:53 +0100

    Cleanup and long version of IQ output (much code duplication)

--- Libraries/si5351/Inc/si5351.h
+++ Libraries/si5351/Inc/si5351.h
@@ -7,6 +7,9 @@
 
 si5351_status_t si5351_Setup(si5351_t* si5351, si5351_bus_t busAdaptor, uint8_t i2cAddr, uint32_t xtalFrequency, int32_t xtalCorrection);
 si5351_status_t si5351_Init(si5351_t* si5351);
+si5351_status_t si5351_SetOutputCurrent(si5351_t* si5351, si5351_clk_num_t clk, si5351_clk_output_current_t outputCurrent);
 
 si5351_status_t si5351_SetFrequency(si5351_t* si5351, si5351_pll_num_t pll, si5351_clk_num_t clk, uint32_t frequency);
+si5351_status_t si5351_SetFrequencyIQ(si5351_t* si5351, si5351_pll_num_t pll, si5351_clk_num_t clkA, si5351_clk_num_t clkB, uint32_t frequency);
+si5351_status_t si5351_SetFrequencyInv(si5351_t* si5351, si5351_pll_num_t pll, si5351_clk_num_t clkA, si5351_clk_num_t clkB, uint32_t frequency);
 #endif
--- Libraries/si5351/Src/si5351.c
+++ Libraries/si5351/Src/si5351.c
@@ -91,13 +91,9 @@ si5351_status_t si5351_Init(si5351_t* si5351)
     commResult = si5351_WriteReg(si5351, SI5351_REG_SPREAD_SPECTRUM_PARAMS_1, regVal);
     if(commResult != SI5351_OK) return commResult;
 
+    // Setting up the clock register (needed because CLKx should use MSx for output)
     for(uint8_t i = 0; i <= 2; i++)
-    {
-        si5351->clks[i].powerDown = false;
-        si5351->clks[i].integerMode = true;
-        si5351->clks[i].msPLLSource = SI5351_PLLA;
-        si5351->clks[i].inverted = false;
-        si5351->clks[i].outputCurrent = SI5351_OUT_8MA;
+    {   
         commResult = si5351_WriteClkCtrlRegister(si5351, i);
         if(commResult != SI5351_OK) return commResult;
     }
@@ -118,7 +114,18 @@ si5351_status_t si5351_SetFrequency(si5351_t* si5351, si5351_pll_num_t pll, si53
 {
     if (!si5351) return SI5351_EINVAL;
 
-    // Finding an even out divider that 
+    si5351_status_t commResult = SI5351_OK;
+    if(!si5351->clks[clk].integerMode || si5351->clks[clk].msPLLSource != pll || si5351->clks[clk].inverted)
+    {
+        si5351->clks[clk].integerMode = true;
+        si5351->clks[clk].msPLLSource = pll;
+        si5351->clks[clk].inverted = false;
+        commResult = si5351_WriteClkCtrlRegister(si5351, clk);
+
+        if(commResult != SI5351_OK) return commResult;
+    }
+
+    // Finding an even out divider 
     uint32_t outDivider = SI5351_PLL_MAX_FREQ / frequency;
     if(outDivider % 2) outDivider--;
 
@@ -149,7 +156,6 @@ si5351_status_t si5351_SetFrequency(si5351_t* si5351, si5351_pll_num_t pll, si53
     };
 
     // Checking whether updating the CLK's MultiSynth (OMD) is necessary (PLL reset needed)
-    si5351_status_t commResult = SI5351_OK;
     if(outDivider != si5351->clks[clk].outDivider)
     {
         // Updating the outDivider
@@ -183,7 +189,94 @@ si5351_status_t si5351_SetFrequency(si5351_t* si5351, si5351_pll_num_t pll, si53
 }
 
 
-si5351_status_t si5351_SetFrequencyPhase(si5351_t* si5351, si5351_pll_num_t pll, si5351_clk_num_t clkA, si5351_t clkB, uint32_t frequency, uint8_t phaseOffset)
+si5351_status_t si5351_SetFrequencyIQ(si5351_t* si5351, si5351_pll_num_t pll, si5351_clk_num_t clkA, si5351_clk_num_t clkB, uint32_t frequency)
 {
-    return SI5351_OK;
+    if (!si5351) return SI5351_EINVAL;
+
+    si5351_status_t commResult = SI5351_OK;
+    if(si5351->clks[clkA].integerMode || si5351->clks[clkA].msPLLSource != pll || si5351->clks[clkB].msPLLSource != pll || si5351->clks[clkB].integerMode)
+    {
+        si5351->clks[clkA].integerMode = false;
+        si5351->clks[clkA].msPLLSource = pll;
+        si5351->clks[clkA].inverted = false;
+        si5351->clks[clkB].integerMode = false;
+        si5351->clks[clkB].msPLLSource = pll;
+        si5351->clks[clkB].inverted = false;
+        commResult = si5351_WriteClkCtrlRegister(si5351, clkA);
+        if(commResult != SI5351_OK) return commResult;
+        commResult = si5351_WriteClkCtrlRegister(si5351, clkB);
+        if(commResult != SI5351_OK) return commResult;
+    }
+
+    
+    // Finding an even out divider that has a maximum value of 126
+    uint32_t outDivider = SI5351_PLL_MAX_FREQ / frequency;
+    if(outDivider > 126)
+    {
+        outDivider = 126;
+    }
+    
+    if(outDivider % 2) outDivider--;
+
+    // Calculating the actual frequency of PLLx
+    uint32_t frequencyPLLx = outDivider * frequency;
+    
+    // Calculating (a/b/c) for the (fractional) divider of PLLx's FMD
+    uint8_t a = frequencyPLLx / si5351->xtalFrequency;
+    uint32_t b = (frequencyPLLx % si5351->xtalFrequency) >> 5;
+    uint32_t c = si5351->xtalFrequency >> 5;
+    uint32_t f = (128 * b) / c;
+
+    // Building the register values for PLLx's MultiSynth N (FMD)
+    uint32_t MSNxP1 = 128 * a + f - 512;
+    uint32_t MSNxP2 = 128 * b - f * c;
+    uint32_t MSNxP3 = c;
+
+    uint8_t PLLxMSNxParams[] =
+    {
+    (MSNxP3 & 0xFF00) >> 8,                                     // Bits [15:8] of MSNx_P3 in register 26
+	MSNxP3 & 0xFF,
+    (MSNxP1 & 0x030000L) >> 16,
+    (MSNxP1 & 0xFF00) >> 8,                                     // Bits [15:8]  of MSNx_P1 in register 29
+	MSNxP1 & 0xFF,                                              // Bits [7:0]  of MSNx_P1 in register 30
+	((MSNxP3 & 0x0F0000L) >> 12) | ((MSNxP2 & 0x0F0000) >> 16), // Parts of MSNx_P3 and MSNx_P1
+	(MSNxP2 & 0xFF00) >> 8,                                     // Bits [15:8]  of MSNx_P2 in register 32
+	MSNxP2 & 0xFF                                               // Bits [7:0]  of MSNx_P2 in register 33
+    };
+
+    // Checking whether updating the CLK's MultiSynth (OMD) is necessary (PLL reset needed)
+    if(outDivider != si5351->clks[clkA].outDivider)
+    {
+        // Updating the outDivider
+        si5351->clks[clkA].outDivider = outDivider;
+
+        uint32_t MSxP1 = 128 * outDivider - 512;
+        uint8_t CLKxMSxParams[] =
+	    {
+        0, 								// Bits [15:8] of MS0_P3 (always 0) in register 42
+		1,            					// Bits [7:0]  of MS0_P3 (always 1) in register 43
+		((MSxP1 & 0x030000L) >> 16),    // Bits [17:16] of MSx_P1 in bits [1:0] and R in [7:4] | [3:2]
+		(MSxP1 & 0xFF00) >> 8, 			// Bits [15:8]  of MSx_P1 in register 45
+		MSxP1 & 0xFF,           		// Bits [7:0]  of MSx_P1 in register 46
+		0,             					// Bits [19:16] of MS0_P2 and MS0_P3 are always 0
+		0,                        		// Bits [15:8]  of MS0_P2 are always 0
+		0                        		// Bits [7:0]   of MS0_P2 are always 0
+	    };
+
+        commResult = si5351_WriteRegs(si5351, SI5351_REG_MULTISYNTH_NA_PARAMS_BASE + (pll * SI5351_REG_MULTISYNTH_NA_PARAMS_NB_PARAMS_OFFSET), PLLxMSNxParams, 8);
+        if(commResult != SI5351_OK) return commResult;
+        commResult = si5351_WriteRegs(si5351, SI5351_REG_MULTISYNTH0_PARAMS + (SI5351_REG_MULTISYNTHx_PARAMS_OFFSET * clkA), CLKxMSxParams, 8);
+        if(commResult != SI5351_OK) return commResult;
+        commResult = si5351_WriteRegs(si5351, SI5351_REG_MULTISYNTH0_PARAMS + (SI5351_REG_MULTISYNTHx_PARAMS_OFFSET * clkB), CLKxMSxParams, 8);
+        if(commResult != SI5351_OK) return commResult;
+        commResult = si5351_WriteReg(si5351, SI5351_REG_CLK0_PHASE_OFFSET + clkB, outDivider);
+        if(commResult != SI5351_OK) return commResult;
+        commResult = si5351_ResetPll(si5351, pll);
+    }
+    else
+    {
+        commResult = si5351_WriteRegs(si5351, SI5351_REG_MULTISYNTH_NA_PARAMS_BASE + (pll * SI5351_REG_MULTISYNTH_NA_PARAMS_NB_PARAMS_OFFSET), PLLxMSNxParams, 8);
+    }
+
+    return commResult;
 }
